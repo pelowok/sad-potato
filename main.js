fb8c23a97dd6eac5e048312689eb9e52b2496c76
pelowok <pelowok@users.noreply.github.com>
2023-10-29 03:01:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementById('playerSlider').addEventListener('input', (event) => {
   const selectedPlayers = event.target.value;
   document.getElementById('playerCount').textContent = `Number of Players: ${selectedPlayers}`;
 
-  const imageSrc = `${selectedPlayers}p.jpg`; // Construct the image filename
+  const imageSrc = `./img/${selectedPlayers}p.jpg`; // Construct the image filename
   document.getElementById('playerImage').src = imageSrc; // Set the image src attribute
 });
 
