9abf1f9a1fac55e9b63434d8be9e04f66db9b7ed
pelowok <pelowok@users.noreply.github.com>
2023-10-29 02:52:26 -0400

    adding images

--- main.js
+++ main.js
@@ -3,4 +3,8 @@
 document.getElementById('playerSlider').addEventListener('input', (event) => {
   const selectedPlayers = event.target.value;
   document.getElementById('playerCount').textContent = `Number of Players: ${selectedPlayers}`;
+
+  const imageSrc = `${selectedPlayers}p.jpg`; // Construct the image filename
+  document.getElementById('playerImage').src = imageSrc; // Set the image src attribute
 });
+
